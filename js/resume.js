4492459be603c4e55f477677ba3835a7b072225a
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-07-23 18:50:59 +0300

    asf

--- js/resume.js
+++ js/resume.js
@@ -1,9 +1,6 @@
 const resumeMoreInfoButtons = document.querySelectorAll('.resume_card-box_card_header img'),
   resumeMoreInfoButtonsClose = document.querySelectorAll('.resume_card-box_card_info img')
 
-console.log(resumeMoreInfoButtons)
-console.log(resumeMoreInfoButtonsClose)
-
 resumeMoreInfoButtons.forEach(resumeMoreInfoButton => {
   resumeMoreInfoButton.addEventListener('click', e => {
     e.target.parentNode.parentNode.children[0].style.display = 'flex'
