332ac810b43b41dd30996cb9f1548864bfe9c5c6
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-07-23 17:27:35 +0300

    1234567894444444

---
+++ js/resume.js
@@ -0,0 +1,17 @@
+const resumeMoreInfoButtons = document.querySelectorAll('.resume_card-box_card_header img'),
+  resumeMoreInfoButtonsClose = document.querySelectorAll('.resume_card-box_card_info img')
+
+console.log(resumeMoreInfoButtons)
+console.log(resumeMoreInfoButtonsClose)
+
+resumeMoreInfoButtons.forEach(resumeMoreInfoButton => {
+  resumeMoreInfoButton.addEventListener('click', e => {
+    e.target.parentNode.parentNode.children[0].style.display = 'flex'
+  })
+})
+
+resumeMoreInfoButtonsClose.forEach(resumeMoreInfoButtonClose => {
+  resumeMoreInfoButtonClose.addEventListener('click', e => {
+    e.target.parentNode.parentNode.children[0].style.display = 'none'
+  })
+})
