88b14827cf7c50147019cc2ce50bef75ac53da95
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-07-25 02:02:09 +0300

    faq

--- js/faq.js
+++ js/faq.js
@@ -3,17 +3,16 @@ const faqItems = document.querySelectorAll('.faq_acordeon-box_item')
 faqItems.forEach(faqItem => {
   faqItem.addEventListener('click', e => {
     if (e.target.nodeName === 'LI') {
-      if (e.target.children[1].style.display === 'block') {
-        e.target.children[1].style.display = 'none'
+      if (e.target.children[1].style.maxHeight) {
+        e.target.children[1].style.maxHeight = ''
       } else {
-        e.target.children[1].style.display = 'block'
+        e.target.children[1].style.maxHeight = '500px'
       }
     } else {
-
-      if (e.target.parentNode.children[1].style.display === 'block') {
-        e.target.parentNode.children[1].style.display = 'none'
+      if (e.target.parentNode.children[1].style.maxHeight) {
+        e.target.parentNode.children[1].style.maxHeight = ''
       } else {
-        e.target.parentNode.children[1].style.display = 'block'
+        e.target.parentNode.children[1].style.maxHeight = '500px'
       }
     }
   })
