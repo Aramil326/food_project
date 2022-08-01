ba5dc3da2208f43e02d90fc5e74e7f21bd17a244
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-01 19:33:05 +0300

    из бавлов

--- js/quiz.js
+++ js/quiz.js
@@ -17,18 +17,19 @@ const cardMan = document.querySelector('.quiz_select-sex_card-box_card[data-sex=
 quizSelectSexHeader = document.querySelector('.quiz_select-sex_header')
 
 let flagMan = false,
-  flagWoman = false
+  flagWoman = false, sex
 
 cardMan.addEventListener('click', () => {
   if (!flagMan && !flagWoman) {
     flagMan = true
     cardMan.classList.add('quiz-active')
-    cardMan
+    sex = 'man'
   } else if (flagMan && !flagWoman) {
     flagMan = false
     cardMan.classList.remove('quiz-active')
   } else if (!flagMan && flagWoman) {
     flagMan = true
+    sex = 'man'
     cardMan.classList.add('quiz-active')
     flagWoman = false
     cardWoman.classList.remove('quiz-active')
@@ -39,6 +40,7 @@ cardWoman.addEventListener('click', () => {
   if (!flagMan && !flagWoman) {
     flagWoman = true
     cardWoman.classList.add('quiz-active')
+    sex = 'woman'
   } else if (!flagMan && flagWoman) {
     flagWoman = false
     cardWoman.classList.remove('quiz-active')
@@ -47,6 +49,7 @@ cardWoman.addEventListener('click', () => {
     cardWoman.classList.add('quiz-active')
     flagMan = false
     cardMan.classList.remove('quiz-active')
+    sex = 'woman'
   }
 })
 
@@ -64,18 +67,39 @@ quiz1ButtonNext.addEventListener('click', () => {
 })
 
 // Вторая секция
-const quizQuestionsHeader = document.querySelector('.quiz_questions_header'),
+let quizQuestionsHeader = document.querySelector('.quiz_questions_header'),
   questionsCards = document.querySelectorAll('.quiz_questions_card-box_wrap .card'),
   quiz2ButtonPrev = document.querySelector('#quiz_2 .buttons .previous'),
-  quiz2ButtonNext = document.querySelector('#quiz_2 .buttons .next')
+  quiz2ButtonNext = document.querySelector('#quiz_2 .buttons .next'),
+  activityCoefficient
 
-questionsCards.forEach(card => {
+if (window.innerWidth <= 768) {
+  questionsCards = document.querySelectorAll('.quiz_questions_card-box .card')
+}
+questionsCards.forEach((card, i) => {
   card.addEventListener('click', () => {
     questionsCards.forEach(card => {
       if (card.classList.contains('quiz-active')) {
         card.classList.remove('quiz-active')
       }
     })
+    switch (i) {
+      case 1:
+        activityCoefficient = 1.2
+        break;
+      case 2:
+        activityCoefficient = 1.3
+        break;
+      case 3:
+        activityCoefficient = 1.5
+        break;
+      case 4:
+        activityCoefficient = 1.8
+        break;
+      case 5:
+        activityCoefficient = 2
+        break;
+    }
     card.classList.toggle('quiz-active')
   })
 })
@@ -105,10 +129,14 @@ quiz2ButtonNext.addEventListener('click', () => {
 
 // Третья секция
 
-const quiz3Header = document.querySelector('#quiz_3 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
+let quiz3Header = document.querySelector('#quiz_3 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
   quiz3Cards = document.querySelectorAll('#quiz_3 .quiz_questions_meat_card-box .quiz_questions_meat_card-box_wrap .card'),
   quiz3ButtonPrev = document.querySelector('#quiz_3 .buttons .previous'),
   quiz3ButtonNext = document.querySelector('#quiz_3 .buttons .next')
+
+if (window.innerWidth <= 768) {
+  quiz3Cards = document.querySelectorAll('#quiz_3 .quiz_questions_meat_card-box .card')
+}
 
 quiz3Cards.forEach(card => {
   card.addEventListener('click', () => {
@@ -145,10 +173,14 @@ quiz3ButtonNext.addEventListener('click', () => {
 })
 
 // Четвертая секция
-const quiz4Header = document.querySelector('#quiz_4 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
+let quiz4Header = document.querySelector('#quiz_4 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
   quiz4Cards = document.querySelectorAll('#quiz_4 .quiz_questions_meat_card-box .quiz_questions_meat_card-box_wrap .card'),
   quiz4ButtonPrev = document.querySelector('#quiz_4 .buttons .previous'),
   quiz4ButtonNext = document.querySelector('#quiz_4 .buttons .next')
+
+if (window.innerWidth <= 768) {
+  quiz4Cards = document.querySelectorAll('#quiz_4 .quiz_questions_meat_card-box .card')
+}
 
 quiz4Cards.forEach(card => {
   card.addEventListener('click', () => {
@@ -185,10 +217,14 @@ quiz4ButtonNext.addEventListener('click', () => {
 })
 
 // Пятая секция
-const quiz5Header = document.querySelector('#quiz_5 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
+let quiz5Header = document.querySelector('#quiz_5 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
   quiz5Cards = document.querySelectorAll('#quiz_5 .quiz_questions_meat_card-box .quiz_questions_meat_card-box_wrap .card'),
   quiz5ButtonPrev = document.querySelector('#quiz_5 .buttons .previous'),
   quiz5ButtonNext = document.querySelector('#quiz_5 .buttons .next')
+
+if (window.innerWidth <= 768) {
+  quiz5Cards = document.querySelectorAll('#quiz_5 .quiz_questions_meat_card-box .card')
+}
 
 quiz5Cards.forEach(card => {
   card.addEventListener('click', () => {
@@ -225,10 +261,14 @@ quiz5ButtonNext.addEventListener('click', () => {
 })
 
 // Шестая секция
-const quiz6Header = document.querySelector('#quiz_6 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
+let quiz6Header = document.querySelector('#quiz_6 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
   quiz6Cards = document.querySelectorAll('#quiz_6 .quiz_questions_meat_card-box .quiz_questions_meat_card-box_wrap .card'),
   quiz6ButtonPrev = document.querySelector('#quiz_6 .buttons .previous'),
   quiz6ButtonNext = document.querySelector('#quiz_6 .buttons .next')
+
+if (window.innerWidth <= 768) {
+  quiz6Cards = document.querySelectorAll('#quiz_6 .quiz_questions_meat_card-box .card')
+}
 
 quiz6Cards.forEach(card => {
   card.addEventListener('click', () => {
@@ -265,10 +305,14 @@ quiz6ButtonNext.addEventListener('click', () => {
 })
 
 // Седьмая секция
-const quiz7Header = document.querySelector('#quiz_7 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
+let quiz7Header = document.querySelector('#quiz_7 .quiz_questions_meat_wrap .quiz_questions_meat_header'),
   quiz7Cards = document.querySelectorAll('#quiz_7 .quiz_questions_meat_card-box .quiz_questions_meat_card-box_wrap .card'),
   quiz7ButtonPrev = document.querySelector('#quiz_7 .buttons .previous'),
   quiz7ButtonNext = document.querySelector('#quiz_7 .buttons .next')
+
+if (window.innerWidth <= 768) {
+  quiz7Cards = document.querySelectorAll('#quiz_7 .quiz_questions_meat_card-box .card')
+}
 
 quiz7Cards.forEach(card => {
   card.addEventListener('click', () => {
@@ -310,55 +354,57 @@ const quiz8Header = document.querySelector('#quiz_8 .parametrs_wrap .parametrs_h
   quiz8ButtonPrev = document.querySelector('#quiz_8 .buttons .previous'),
   quiz8ButtonNext = document.querySelector('#quiz_8 .buttons .next')
 
+let age, currentWeight, height, desiredWeight
+
 quiz8Inputs[0].addEventListener('change', (e) => {
-  console.log(e.target.value)
   if (e.target.value < 5 || e.target.value > 100) {
     e.target.style.border = '1px solid red'
     setTimeout(() => {
       e.target.style.border = '1px solid #585858'
       e.target.value = ''
-    }, 1000)
+    }, 500)
   } else {
     e.target.style.border = '1px solid green'
+    age = e.target.value
   }
 })
 
 quiz8Inputs[1].addEventListener('change', (e) => {
-  console.log(e.target.value)
   if (e.target.value < 30) {
     e.target.style.border = '1px solid red'
     setTimeout(() => {
       e.target.style.border = '1px solid #585858'
       e.target.value = ''
-    }, 1000)
+    }, 500)
   } else {
     e.target.style.border = '1px solid green'
+    currentWeight = e.target.value
   }
 })
 
 quiz8Inputs[2].addEventListener('change', (e) => {
-  console.log(e.target.value)
   if (e.target.value < 110 || e.target.value > 250) {
     e.target.style.border = '1px solid red'
     setTimeout(() => {
       e.target.style.border = '1px solid #585858'
       e.target.value = ''
-    }, 1000)
+    }, 500)
   } else {
     e.target.style.border = '1px solid green'
+    height = e.target.value
   }
 })
 
 quiz8Inputs[3].addEventListener('change', (e) => {
-  console.log(e.target.value)
   if (e.target.value < 30) {
     e.target.style.border = '1px solid red'
     setTimeout(() => {
       e.target.style.border = '1px solid #585858'
       e.target.value = ''
-    }, 1000)
+    }, 500)
   } else {
     e.target.style.border = '1px solid green'
+    desiredWeight = e.target.value
   }
 })
 
@@ -426,16 +472,12 @@ function analysis() {
     checkpoint3 = document.querySelector('.checkpoints li img[data-check="3"]'),
     checkpoint4 = document.querySelector('.checkpoints li img[data-check="4"]')
 
-
-
   function checkpointsRotate(checkpoint) {
     let start = Date.now();
 
     let timer = setInterval(function () {
       let timePassed = Date.now() - start;
-
       checkpoint.style.transform = `rotate(${timePassed / 5}deg)`;
-
       if (timePassed > 2500) {
         clearInterval(timer)
         checkpoint.style.transform = `rotate(0deg)`;
@@ -445,7 +487,6 @@ function analysis() {
   }
 
   checkpointsRotate(checkpoint1)
-
   setTimeout(() => checkpointsRotate(checkpoint2), 2500);
   setTimeout(() => checkpointsRotate(checkpoint3), 5000);
   setTimeout(() => checkpointsRotate(checkpoint4), 7500);
@@ -454,4 +495,72 @@ function analysis() {
     quizScreen10.style.display = 'flex'
 
   }, 15000);
+  resumeUpdate()
 }
+
+function resumeUpdate() {
+  let imt = document.querySelector('.resume_card-box_card_title'),
+    imtImg = document.querySelector('.resume_imt_img'),
+    imtSubtitle = document.querySelector('.resume_imt_subtitle'),
+    metabolicAge
+
+  height = height / 100
+  imt.textContent = Math.round(currentWeight / (height * height))
+
+  if (imt.textContent < 18.5) {
+    imtImg.src = 'img/scale-1.png'
+    imtSubtitle.textContent = 'Недостаточная масса тела'
+    metabolicAge = age - 5
+  } else if (imt.textContent > 18.5 && imt.textContent < 25) {
+    imtImg.src = 'img/scale-2.png'
+    imtSubtitle.textContent = 'Нормальная масса тела'
+    metabolicAge = age
+  } else if (imt.textContent > 25 && imt.textContent < 30) {
+    imtImg.src = 'img/scale-3.png'
+    imtSubtitle.textContent = 'Избыточная масса тела'
+    metabolicAge = age + 5
+  } else if (imt.textContent > 30 && imt.textContent < 35) {
+    imtImg.src = 'img/scale-4.png'
+    imtSubtitle.textContent = 'Ожирение 1-ой степени'
+    metabolicAge = age + 10
+  } else if (imt.textContent > 35) {
+    imtImg.src = 'img/scale-5.png'
+    imtSubtitle.textContent = 'Ожирение'
+    metabolicAge = age + 15
+  }
+
+  // --------------------------------------------------------------
+  let metabolicAgeImg = document.querySelector('.resume_card-box_card_girl-boy'),
+    metabolicAgeSubtitle = document.querySelector('.metabolicAgeSubtitle')
+
+  function plural(number, titles) {
+    cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  }
+  const declension = ['год', 'года', 'лет'];
+
+  metabolicAgeSubtitle.textContent = `${metabolicAge} ${plural(metabolicAge, declension)}`
+
+  if (sex === 'man') {
+    if (metabolicAge < 18) {
+      metabolicAgeImg.src = 'img/resume-boys-1.png'
+    } else if (metabolicAge > 18 || metabolicAge < 50) {
+      metabolicAgeImg.src = 'img/resume-boys-2.png'
+    } else {
+      metabolicAgeImg.src = 'img/resume-boys-3.png'
+    }
+  } else if (sex === 'woman') {
+    if (metabolicAge < 18) {
+      metabolicAgeImg.src = 'img/resume-girls-1.png'
+    } else if (metabolicAge > 18 || metabolicAge < 50) {
+      metabolicAgeImg.src = 'img/resume-girls-2.png'
+    } else {
+      metabolicAgeImg.src = 'img/resume-girls-3.png'
+    }
+  }
+
+  // --------------------------------------------------------------
+  console.log(activityCoefficient)
+
+}
+
---
+++ send.php
@@ -0,0 +1,183 @@
+<?php header("Content-Type: text/html; charset=utf-8");
+$recaptcha  = $_POST['g-recaptcha-response'];
+
+if (!empty($recaptcha)) {
+  $recaptcha = $_REQUEST['g-recaptcha-response'];
+  $secret = '6Lcf4SohAAAAAI0G4vjpFB_iSkA7Hj5UprRP3w7S';
+  $ip = $_SERVER['REMOTE_ADDR'];
+  $google_url = "https://www.google.com/recaptcha/api/siteverify";
+  $url = $google_url . "?secret=" . $secret . "&response=" . $recaptcha . "&remoteip=" . $ip;
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+  curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0(Windows;U;WindowsNT6.1;en-US;rv:1.9.2.16)Gecko/20110319Firefox/3.6.16");
+  $curlData = curl_exec($curl);
+  curl_close($curl);
+  $res = json_decode($curlData, true);
+  //if($curlData['success']) {
+  if ($res['success']) {
+    $fMail =  'Почта: ' . $_POST["email"] . ' <br />';
+    $fTitle =  'Заголовок: ' . $_POST["title"] . ' <br />';
+    $fMessage =  'Сообщение: ' . $_POST["message"] . ' <br />';
+    $AllInOne =  $fMail . $fTitle . $fMessage;
+    $to = 'ketotoday@yandex.ru';
+    $headers = "From:https://hardworking-beaver.ru <site@hardworking-beaver.ru>\nReply-to:ketotoday@yandex.ru\nContent-Type: text/html; charset=\"utf-8\"\n";
+    if (mail($to, 'Сообщение от пользователя', $AllInOne, $headers)) {
+      header('Refresh: 7; URL=https://keto-day/contact.html');
+      echo '
+      <!DOCTYPE html>
+      <html lang="en">
+      
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css">
+      
+        <!-- Стили секций -->
+        <link rel="stylesheet" href="css/answer.css">
+      
+        <!-- Шрифты -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet">
+      
+        
+      
+        <title>KETO-DAY</title>
+      </head>
+      
+      <body>
+        <section class="answer">
+          <div class="answer_wrap">
+            <h1>Ваше сообщение отправлено</h1>
+            <h2>Скоро мы на него ответим</h2>
+          </div>
+        </section>
+        
+      </body>
+      
+      </html>';
+    } else {
+      header('Refresh: 7; URL=https://keto-day/contact.html');
+      echo '<!DOCTYPE html>
+      <html lang="en">
+      
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css">
+      
+        <!-- Стили секций -->
+        <link rel="stylesheet" href="css/answer.css">
+      
+        <!-- Шрифты -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet">
+      
+        
+      
+        <title>KETO-DAY</title>
+      </head>
+      
+      <body>
+        <section class="answer">
+          <div class="answer_wrap">
+            <h1>Что-то пошло не так :(</h1>
+            <h2>Попробуйте снова еще раз</h2>
+          </div>
+        </section>
+        
+      </body>
+      
+      </html>';
+    }
+  } else {
+    header('Refresh: 7; URL=https://keto-day/contact.html');
+    echo '<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+      <link rel="stylesheet" href="css/index.css">
+      <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css">
+    
+      <!-- Стили секций -->
+      <link rel="stylesheet" href="css/answer.css">
+    
+      <!-- Шрифты -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    
+      
+    
+      <title>KETO-DAY</title>
+    </head>
+    
+    <body>
+      <section class="answer">
+        <div class="answer_wrap">
+          <h1>Подтвердите, что вы не робот и попробуйте еще раз</h1>
+        </div>
+      </section>
+      
+    </body>
+    
+    </html>';
+  }
+} else {
+  header('Refresh: 7; URL=https://keto-day/contact.html');
+  echo '<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset.css">
+  
+    <!-- Стили секций -->
+    <link rel="stylesheet" href="css/answer.css">
+  
+    <!-- Шрифты -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet">
+  
+    
+  
+    <title>KETO-DAY</title>
+  </head>
+  
+  <body>
+    <section class="answer">
+      <div class="answer_wrap">
+        <h1>Поставьте галочку в поле "Я не робот" для отправки сообщения и попробуйте еще раз</h1>
+      </div>
+    </section>
+    
+  </body>
+  
+  </html>';
+}
